1ed49ca19d3d50aa65a5d6c65653258205fe6cec
Aiden Tabrah <115892124+IsaacSShade@users.noreply.github.com>
2024-11-02 16:45:36 -0700

    Changes car blue

--- global-colding/Sub-scenes/Ice Bolt/ice_bolt_movement.gd
+++ global-colding/Sub-scenes/Ice Bolt/ice_bolt_movement.gd
@@ -5,6 +5,9 @@ const SPEED = 400
 func _ready():
 	pass
 
-
 func _physics_process(delta):
 	position += transform.x * SPEED * delta
+
+func _on_body_entered(body: Node) -> void:
+	if body.name == "Car":
+		body.freeze()
