4a139c3bb4a562dcc5e8bb35b76a01870f665bc6
izzytilles <123509336+izzytilles@users.noreply.github.com>
2024-11-02 17:36:06 -0700

    more temp stuff

--- global-colding/Sub-scenes/Temperature Label/temperature.gd
+++ global-colding/Sub-scenes/Temperature Label/temperature.gd
@@ -1,18 +1,30 @@
-#extends Control
-#
-## Variables that initialize when TemperatureBar is called
-#onready var temperature_bar = $TemperatureBar
-#onready var object_detector = $ObjectDetector
-##
-### base temp is 70 degrees
-##var temperature = 70
-##var temp_change_rate = 5
-##
-### Called every frame. 'delta' is the elapsed time since the previous frame.
-##func _process(delta: float) -> void:
-	##pass
-##
-##func get_current_temp:
-	##
-##func get_object_temp:
+extends Control
+
+# Define the temperature bar and area detection
+@onready var temperature_bar = $ProgressBar
+@onready var area_detector = $Area2D
+
+# Temperature parameters
+var current_temperature = 50  # Starting temperature
+var target_temperature = 50   # Desired temperature
+var temperature_change_rate = 5  # Rate of temperature change per detected object
+var transition_speed = 5.0  # Speed of the transition (higher = faster)
+
+func _on_body_entered(body):
+	# Increase target temperature when an object enters the area
+	if body.is_in_group("heat_sources"):
+		target_temperature += temperature_change_rate
+
+func _on_body_exited(body):
+	# Decrease target temperature when an object exits the area
+	if body.is_in_group("heat_sources"):
+		target_temperature -= temperature_change_rate
+
+func _process(delta):
+	# Smoothly transition the current temperature towards the target temperature
+	current_temperature = lerp(current_temperature, target_temperature, transition_speed * delta)
+	
+	# Clamp the current temperature value and update the ProgressBar
+	current_temperature = clamp(current_temperature, 0, temperature_bar.max_value)
+	temperature_bar.value = current_temperature
 	
